3ec515c38f2027b2a3b2f15477a1d7d973f24aa0
Alexey Zakharenko <Alexey.Zakharenko@gmail.com>
2022-12-29 09:31:33 +0300

    Add google

---
+++ Google.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import httplib2
+
+CLIENT_SECRET_FILE = 'google-client-secret.json'
+API_NAME = 'photoslibrary'
+API_VERSION = 'v1'
+SCOPES = ['https://www.googleapis.com/auth/photoslibrary']
+
+
+h = http.Http()
+
